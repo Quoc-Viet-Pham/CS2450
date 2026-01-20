3d862ec3405ca15aef11247f1cf44fdf1ccfbb60
Quoc Viet Pham <d00521458@utahtech.edu>
2026-01-20 10:09:38 -0700

    Avoid guessing same age twice

--- age_guesser.py
+++ age_guesser.py
@@ -1,17 +1,45 @@
+# # import random
+
+# # print("Hi! I'm going to try to guess your age.")
+# # name = input("What's your name? ")
+
+# # while True:
+# #     age_guess = random.randint(15, 30)
+# #     answer = input(f"Are you {age_guess} years old? (y/n): ").strip().lower()
+
+# #     if answer == 'y':
+# #         print(f"Yes! {name} is {age_guess} years old.")
+# #         break
+# #     else:
+# #         print("Rats.")
+
 # import random
 
 # print("Hi! I'm going to try to guess your age.")
 # name = input("What's your name? ")
 
-# while True:
-#     age_guess = random.randint(15, 30)
+# possible_ages = list(range(15, 31))
+# attempts = 0
+
+# while possible_ages:
+#     age_guess = random.choice(possible_ages)
+#     possible_ages.remove(age_guess)
+#     attempts += 1
+
 #     answer = input(f"Are you {age_guess} years old? (y/n): ").strip().lower()
 
 #     if answer == 'y':
 #         print(f"Yes! {name} is {age_guess} years old.")
+#         print(f"I guessed it in {attempts} tries!")
 #         break
-#     else:
+#     elif answer == 'n':
 #         print("Rats.")
+#     else:
+#         print("Please enter 'y' or 'n'.")
+#         possible_ages.append(age_guess)  # put it back if input was invalid
+
+# else:
+#     print("Hmm... I ran out of ages to guess. Are you sure you’re between 15 and 30?")
 
 import random
 
@@ -19,24 +47,22 @@ print("Hi! I'm going to try to guess your age.")
 name = input("What's your name? ")
 
 possible_ages = list(range(15, 31))
-attempts = 0
 
-while possible_ages:
+while True:
+    if not possible_ages:
+        print("I ran out of ages to guess! Are you really between 15 and 30?")
+        break
+
     age_guess = random.choice(possible_ages)
     possible_ages.remove(age_guess)
-    attempts += 1
 
     answer = input(f"Are you {age_guess} years old? (y/n): ").strip().lower()
 
     if answer == 'y':
         print(f"Yes! {name} is {age_guess} years old.")
-        print(f"I guessed it in {attempts} tries!")
         break
     elif answer == 'n':
         print("Rats.")
     else:
-        print("Please enter 'y' or 'n'.")
+        print("Please answer with 'y' or 'n'.")
         possible_ages.append(age_guess)  # put it back if input was invalid
-
-else:
-    print("Hmm... I ran out of ages to guess. Are you sure you’re between 15 and 30?")
